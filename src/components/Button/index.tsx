3791821e07b010e13a5b939e6c3ccd0daa47c9e8
Stas Sytnykov <stas82kak05@gmail.com>
2024-12-19 20:26:57 +0200

    Add two basic components Button and SurveyScreen

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+  onClick: () => void;
+}
+
+export const Button = ({ children, onClick }: ButtonProps) => {
+  return <button onClick={onClick}>{children}</button>;
+};
---
+++ src/components/SurveyScreen/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { QuestionData } from "@/src/types";
+import { Button } from "../Button";
+import { useRouter } from "next/navigation";
+
+interface SurveyScreenProps {
+  questionData: QuestionData;
+}
+
+export const SurveyScreen = ({ questionData }: SurveyScreenProps) => {
+  const router = useRouter();
+
+  const handleClick = (nextQuestion: string) => {
+    router.push(nextQuestion);
+  };
+
+  return (
+    <div>
+      {questionData.question}
+      {questionData.options.map((option) => (
+        <Button key={option.label} onClick={() => handleClick(option.next)}>
+          {option.label}
+        </Button>
+      ))}
+    </div>
+  );
+};
